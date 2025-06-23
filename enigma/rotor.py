0eaf59beb49ffa87f7b5209eb6310e9dfba6960e
Andrew Gattineri <andrew@arad5.com>
2025-06-23 18:40:22 -0400

    Merged the two workingfile sets. worked database code to write to database after encryption.

--- enigma/rotor.py
+++ enigma/rotor.py
@@ -1,30 +1,33 @@
 class Rotor:
-    def __init__(self):
-        self.name = "Default Rotor"
-        self.letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-                       'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-                       'u', 'v', 'w', 'x', 'y', 'z']
-        self.offset = 0;
+    def __init__(self, offset=0):
+
+        # Cleaner implementation from https://stackoverflow.com/questions/16060899/alphabet-range-in-python
+        # Creates an array of all of the letters
+        self.letters = [chr(i + ord('a')) for i in range (26)]
+        self.offset = offset % 26
     
-    def encrypt(self):
-        message = input("What is your message you want to encrypt? ").lower()
-        self.offset = int(input("How big would you like your offset? (Ex: 1 shifts A->B): "))
-        encrypted_message = ""
-        
-        for char in message:
+    def set_offset(self, offset: int):
+        # Set the rotor offset (0-25)
+        self.offset = offset % 26
+    
+    def get_offset(self) -> int:
+        # Get rotor offset for debug
+        return self.offset
+    
+    def encrypt(self, message: str) -> str:
+        encrypted_message = []
+        for char in message.lower():
             if char in self.letters:
                 # Current index spot
                 current_pos = self.letters.index(char)
                 # Calculate new position with wraparound
                 new_pos = (current_pos + self.offset) % 26
-                encrypted_message += self.letters[new_pos]
+                encrypted_message.append(self.letters[new_pos])
             else:
                 # Encrypt Message, add new letters to new value
-                encrypted_message += char
-        
-        print(f"Original: {message}")
-        print(f"Encrypted: {encrypted_message}")
-        return encrypted_message
+                encrypted_message.append(char)
+                
+        return ''.join(encrypted_message)
     
     def show_position(self):
         return self.offset
