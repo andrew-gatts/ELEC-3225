fafa86886f44cfbb79afcf112b28f6ba5b84b93a
= <=>
2025-07-09 14:17:23 -0400

    main and plugboard additions

--- enigma/main.py
+++ enigma/main.py
@@ -1,4 +1,5 @@
 from rotor import Rotor
+from plugboard import Plugboard
 from database import init_db, add_entry, close_db
 
 DB_PATH = "database.db"
@@ -6,21 +7,21 @@ DB_PATH = "database.db"
 def main():
     # Initialize Database
     database = init_db()
-    
+    #Testing Rotor Encryption
     message = input("Enter the message to encrypt: ")
     offset = int(input("Enter the rotor offset (1-25): "))
-
     print(f"{offset}")
-
     rotor = Rotor(offset)
     encrypted = rotor.encrypt(message)
-
-    crn = add_entry(database, message, encrypted)
-    print(f"Saved entry CRN={crn}")
-
     print(f"Original : {message}")
     print(f"Encrypted: {encrypted}")
 
+
+    plugboard1 = Plugboard()
+    plugboard1.swap()
+
+    crn = add_entry(database, message, encrypted)
+    print(f"Saved entry CRN={crn}")
     close_db(database)
 
 main()  
--- enigma/plugboard.py
+++ enigma/plugboard.py
@@ -7,14 +7,29 @@ class Plugboard():
                        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
                        'u', 'v', 'w', 'x', 'y', 'z']
         
-    def setting1(self):
-        swap1 = input("What letter would you like to swap?")
-        print (f'What letter would you like to swap  {swap1}  with?')
-        swap2 = input(f'You are swapping {swap1} with {swap2}')
+    def swap(self):
+        choice = int((input("What would you like to do? \n 1.)Swap Letters \n 2.)Exit the Plugboard Config")))
 
-        if swap1 in self.letters and swap2 in self.letters:
-                s = swap2.replace(swap1)
-                return s
+        while choice == 1:
+                swapped_letters = []
+                swap1 = input("What letter would you like to swap?")
+                swap2 = input(f'What letter would you like to swap  {swap1}  with?')
+                print(f'You are swapping {swap1} with {swap2}')
+                if swap1 in self.letters and swap2 in self.letters:
+            # Find positions of the letters
+                    pos1 = self.letters.index(swap1)
+                    pos2 = self.letters.index(swap2)
+                    # Swap them in letters[]
+                    self.letters[pos1], self.letters[pos2] = self.letters[pos2], self.letters[pos1]
+                    # Track the swap
+                    swapped_letters.append((swap1, swap2))
+                    print(f"Swapped {swap1} and {swap2}")
+                    for i in range (0,26):
+                        print(f"{self.letters[i]}")    
+
+                choice = int(input("What would you like to do? \n 1.)Swap Letters \n 2.)Exit the Plugboard Config"))
+                
+        return self.letters
 
 
 
