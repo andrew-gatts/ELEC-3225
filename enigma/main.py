1510ca3819c103a3fec8ce799f087f1b4d7d1403
Andrew Gattineri <andrew@arad5.com>
2025-07-10 10:58:10 -0400

    Moved plugboard first and changed to a function rather than a class

--- enigma/main.py
+++ enigma/main.py
@@ -1,5 +1,5 @@
 from rotor import Rotor
-from plugboard import Plugboard
+from plugboard import plugboard
 from database import init_db, add_entry, close_db
 
 DB_PATH = "database.db"
@@ -9,23 +9,23 @@ def main():
     database = init_db()
     #Testing Rotor Encryption
     message = input("Enter the message to encrypt: ")
-    offset = int(input("Enter the rotor offset (1-25): "))
-    print(f"{offset}")
-    rotor = Rotor(offset)
-    encrypted = rotor.encrypt(message)
-    print(f"Original : {message}")
-    print(f"Encrypted: {encrypted}")
 
     # Plugboard
     swap1 = input("What letter would you like to swap? ")
     swap2 = input(f'What letter would you like to swap {swap1} with? ')
-    # Create plugboard class
-    plugboard1 = Plugboard()
-    pbencrypted = plugboard1.swap(encrypted, swap1, swap2)
+    pbencrypted = plugboard(message, swap1, swap2)
     print(pbencrypted)
+
+    # Rotor
+    offset = int(input("Enter the rotor offset (1-25): "))
+    print(f"{offset}")
+    rotor = Rotor(offset)
+    encrypted = rotor.encrypt(pbencrypted)
+    print(f"Original : {message}")
+    print(f"Encrypted: {encrypted}")
     
     # Database Entry
-    crn = add_entry(database, message, pbencrypted)
+    crn = add_entry(database, message, encrypted)
     print(f"Saved entry CRN={crn}")
     close_db(database)
 
--- enigma/plugboard.py
+++ enigma/plugboard.py
@@ -1,21 +1,15 @@
 # Plugboard Class
-class Plugboard():
-
-    def __init__(self):
-        self.name = "Default Plugboard"
-        # self.letters = [chr(i + ord('a')) for i in range (26)]
-
-    def swap(self, message, swap1, swap2):
-        new_message = []
-        for char in message.lower():
-            if char is swap1:
-                new_message.append(swap2)
-            elif char is swap2:
-                new_message.append(swap1)
-            else:
-                new_message.append(char)
-        
-        return ''.join(new_message)
+def plugboard(message, swap1, swap2):
+    new_message = []
+    for char in message.lower():
+        if char is swap1:
+            new_message.append(swap2)
+        elif char is swap2:
+            new_message.append(swap1)
+        else:
+            new_message.append(char)
+    
+    return ''.join(new_message)
 
 
 
